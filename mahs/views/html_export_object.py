e79c3fbab4bef99896e337d8e7f3325b3aab274b
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-04-16 09:43:53 +0100

    add object template

--- mahs/views/html_export_object.py
+++ mahs/views/html_export_object.py
@@ -10,13 +10,13 @@ from uuid import UUID
 def html_export_object(request):
 
     html_writer = HtmlWriter()
-    resource_list = html_writer.fetch_resource_objects_list(resourceinstanceids=['5e106bab-8993-4f84-9f81-852d30fd07ad'],
-                                                            allowed_graph_ids=['5b144ae2-fc19-11ec-9eef-0050568e7db6'])
+    resource_list = html_writer.fetch_resource_objects_list(resourceinstanceids=['498960a5-ed96-4e95-ad30-b8d218518a74'],
+                                                            allowed_graph_ids=['0340d04e-0801-11ed-9eef-0050568e7db6'])
     
-    resource = resource_list["5b144ae2-fc19-11ec-9eef-0050568e7db6"][0]
+    resource = resource_list["0340d04e-0801-11ed-9eef-0050568e7db6"][0]
 
     return render(
         request,
-        'html_export/site.htm',
+        'html_export/object.htm',
         {"resources": [resource]},
     )
