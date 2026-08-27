13180009a6b806f50292a6b59935b5a70f005447
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-08-27 15:15:57 +0100

    Format settings

--- mahs/settings.py
+++ mahs/settings.py
@@ -424,7 +424,9 @@ SHOW_LANGUAGE_SWITCH = len(LANGUAGES) > 1
 # ES_MAPPING_MODIFIER_CLASSES = ["mahs.search.es_mapping_modifier.EsMappingModifier"]
 
 # Add PDF writer to FORMATTERS
-RESOURCE_FORMATTERS["pdf"] = "arches_pdf_exporting.utils.data_management.resources.formats.pdf.PdfWriter"
+RESOURCE_FORMATTERS["pdf"] = (
+    "arches_pdf_exporting.utils.data_management.resources.formats.pdf.PdfWriter"
+)
 
 try:
     from .package_settings import *
